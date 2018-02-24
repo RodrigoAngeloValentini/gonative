4a2fa234b251c7de092897e8db046ff3e89b6821
Rodrigo Angelo Valentini <rodrigoangelovalentini@hotmail.com>
2018-02-23 21:51:12 -0300

    modulo02

---
+++ modulo02/src/pages/repositories/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+import { metrics } from 'styles';
+
+const styles = StyleSheet.create({
+  loading: {
+    marginTop: metrics.basePadding,
+  },
+
+  listFooter: {
+    height: 10,
+  },
+});
+
+export default styles;
